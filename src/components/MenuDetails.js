cfeb06449bb1abdce912e579043b36a5d6b38718
Kate Bennert <kate.bennert@gmail.com>
2022-11-11 10:48:46 -0500

    Add menu detail card

--- src/components/MenuDetails.js
+++ src/components/MenuDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Nav, Card, Button, CardGroup, ListGroup } from "react-bootstrap";
 
 function MenuDetails({ menus }) {
   
@@ -16,7 +16,24 @@ function MenuDetails({ menus }) {
             <br/>
             <br/>
             <br/>
-            <h3>{menus[index].menuName}</h3>
+            <CardGroup>
+            <Card className="bg-dark text-white" style={{ width: '18rem' }}>
+      <Card.Img src={menus[index].menuImage} alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>{menus[index].menuName}</Card.Title>
+        <Card.Text>
+          {menus[index].menuDescription}
+        </Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Recipes:</ListGroup.Item>
+        <ListGroup.Item>Check out the {menus[index].menuName} playlist on Spotify!</ListGroup.Item>
+        <ListGroup.Item>Shopping List:</ListGroup.Item>
+      </ListGroup>
+    </Card>
+    </CardGroup>
       </Container>
     </div>
   );
