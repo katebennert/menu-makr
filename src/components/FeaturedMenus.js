d03969bc073b45b1c5c12364ed0da6947627df5b
Kate Bennert <kate.bennert@gmail.com>
2022-11-12 13:04:00 -0500

    Add conditional rendering

--- src/components/FeaturedMenus.js
+++ src/components/FeaturedMenus.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Container } from 'react-bootstrap';
 
-function FeaturedMenus() {
+function FeaturedMenus({ menus }) {
+
+    const renderFeaturedCard = (
+        menus.map(menu => (
+            menu.featured ? <p>{menu.menuName}</p> : <></>
+        ))
+    )
+
   return (
     <div>
         <Container>
             <br/>
             <br/>
             <br/>
-        <h1>What's Featured This Fall?</h1>
-
+            <h1>'Tis the Season</h1>
+            <Container>
+                <br/>
+                {renderFeaturedCard}
+            </Container>
         </Container>
     </div>
   )
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,48 +1,31 @@
 import React from "react";
 import { Container, Carousel } from 'react-bootstrap';
 
-function HomePage() {
+// use state to setFeaturedMenus so you can use it in other places?
+
+function HomePage({ menus }) {
+
+    const featuredCarouselSlide = (
+        menus.map(menu => (
+            menu.featured ? <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={menu.menuImage}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>{menu.menuName}</h3>
+                    <p>{menu.menuDescription}</p>
+                </Carousel.Caption>
+            </Carousel.Item> : <></>
+        ))
+    )
+
   return (
     <div>
         <Container>
             <Carousel fade>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://media.istockphoto.com/id/1345496527/photo/senior-woman-saying-grace-before-thanksgiving-lunch-at-home.jpg?b=1&s=170667a&w=0&k=20&c=jCNEhNeVTYNoioRsfyEorZq8DYPElIrzKgnckqqaM0I="
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://media.istockphoto.com/id/1421572711/photo/autumn-composition-green-desk-with-pumpkins-and-eucalyptus-branch-flat-lay-top-view-copy.jpg?b=1&s=170667a&w=0&k=20&c=V09WM3aijFptd5IdvDQz6flUcbnhjSuoxhHe0lWgZ2w="
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://media.istockphoto.com/id/1435123306/photo/stuffed-turkey-for-thanksgiving-holidays.jpg?b=1&s=170667a&w=0&k=20&c=1kOePgO13VeYgd6tBjx96HmbKDLKot6W2B_Cu81r5AI="
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+                {featuredCarouselSlide}
             </Carousel>
             
         </Container>
