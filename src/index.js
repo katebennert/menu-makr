75c60f984e2ee7ee8487523fd68ba369bc2de3a8
Kate Bennert <kate.bennert@gmail.com>
2022-11-18 08:19:45 -0500

    Add ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
