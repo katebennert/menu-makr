24ef4dbd89de5a49e811df94131ecafbd364b041
Kate Bennert <kate.bennert@gmail.com>
2022-11-22 11:16:13 -0500

    Fix delete button

--- src/components/MenusPage.js
+++ src/components/MenusPage.js
@@ -4,14 +4,14 @@ import MenuList from "./MenuList";
 import MenuDetails from "./MenuDetails";
 import { Container } from 'react-bootstrap';
 
-function MenusPage({ menus, dropFilter }) {
+function MenusPage({ menus, dropFilter, onDelete }) {
 
   const match = useRouteMatch();
 
   return (
     <div>
       <Route path={`${match.url}/:id`}>
-        <MenuDetails menus={menus}/>
+        <MenuDetails menus={menus} onDelete={onDelete}/>
       </Route>
       <MenuList menus={menus} dropFilter={dropFilter}/>
     </div>
